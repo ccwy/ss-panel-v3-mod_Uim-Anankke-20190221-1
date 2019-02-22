40e5d80eb7b6d135768fe8f63ba572706327534a
ccwy <ccwy@users.noreply.github.com>
2019-02-22 19:55:07 +0800

    tr

--- app/Services/Gateway/AopF2F.php
+++ app/Services/Gateway/AopF2F.php
@@ -38,7 +38,7 @@ class AopF2F extends AbstractPayment
             $res['msg'] = "订单金额错误：" . $amount;
             return $response->getBody()->write(json_encode($res));
         }
-        if ($amount < $Config::get('codypaymenay')) {
+        if ($amount < (Config::get('codypaymenay'))) {
             $res['ret'] = 0;
             $res['msg'] = "充值金额需大于15元。";
             return $response->getBody()->write(json_encode($res));
