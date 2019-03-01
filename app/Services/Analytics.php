f5a1fa2b061b3fc5bdb8d8e49410eab3c3048f5e
ccwy <ccwy@users.noreply.github.com>
2019-03-01 14:46:39 +0800

    Update Analytics.php

--- app/Services/Analytics.php
+++ app/Services/Analytics.php
@@ -133,7 +133,7 @@ class Analytics
 	//付费用户，排除443端口和管理员、测试账号
     public function classuseraa()
     {
-        return User::where('class', '>', 1)->where('id','!=',1)->where('id','!=',1722)->where('id','!=',1766)->count();
+        return User::where('class', '>', 1)->where('id','!=',2291)->where('id','!=',2293)->count();
     }
 	//统计全站已用总流量
 	public function ssnodeTrafficUsage()
