f5a6222fb82cede058be8187eb0f8a9e4ba73a82
ccwy <ccwy@users.noreply.github.com>
2019-03-05 13:14:51 +0800

    2121

--- public/theme/material/js/fuck.js
+++ public/theme/material/js/fuck.js
@@ -108,6 +108,13 @@
         }
     };
 }));
+
+var browserHTML = '<div style="padding: 15px"><h2>Chrome</h2><p><a href="https://www.google.cn/chrome">前往 Google Chrome 官网下载 Chrome 浏览器（全平台）</a></p><p><a href="https://lab.skk.moe/chrome">Chrome 离线包下载（仅限 Windows）</a></p><p><a href="https://www.wandoujia.com/apps/com.android.chrome">前往豌豆荚下载 Chrome 安卓版</a></p><p><a href="https://play.google.com/store/apps/details?id=com.android.chrome">前往 Google Play 下载 Chrome 安卓版</a></p><p><a href="https://itunes.apple.com/us/app/chrome/id535886823">前往 App Store 下载 Chrome iOS 版</a></p><h2>Firefox</h2><p><a href="https://www.mozilla.org/zh-CN/firefox/">前往 Mozilla 官网下载 Firefox 浏览器（全平台）</a></p><p><a href="https://www.firefox.com.cn/">前往火狐中文网下载 Firefox 浏览器（全平台）</a></p><p><a href="https://www.wandoujia.com/apps/org.mozilla.firefox">前往豌豆荚下载 Firefox 安卓版</a></p><p><a href="https://www.coolapk.com/apk/org.mozilla.firefox">前往酷安下载 Firefox 安卓版</a></p><p><a href="https://play.google.com/store/apps/details?id=org.mozilla.firefox">前往 Google Play 下载 Firefox 安卓版</a></p></div>';
+
+var mqqHTML = '<div style="padding: 15px"><h2>请从菜单中选择「从浏览器中打开」</h2></div>';
+
+var bodyEl = document.getElementsByTagName('body')[0];
+
 // 判断 360 浏览器跳转
 if (new Browser().browser == '360' || new Browser().browser == '360EE' || new Browser().browser == '360SE') {
     window.alert('为了您的人身安全，我们禁止您使用 360 浏览器访问本站，我们要求您通过 Chrome 或 Firefox 浏览器访问本站。\n点击确定后将会跳转至 Chrome 浏览器下载');
@@ -123,8 +130,8 @@ if (new Browser().browser == '360' || new Browser().browser == '360EE' || new Br
 	window.location.href='https://www.google.cn/chrome';
 } else if(new Browser().browser == 'QQ'){
     window.alert('手机 QQ 浏览器内核存在严重缺陷。\n为了您能更好地浏览本站，我们推荐您使用 Chrome 或 Firefox 浏览器。');
-    window.location.href='https://www.google.cn/chrome';
+    bodyEl.innerHTML = mqqHTML;
 } else if(new Browser().browser == 'Wechat'){
     window.alert('为了您的人身安全，我们禁止您使用微信访问本站，我们推荐您通过 Chrome 或 Firefox 浏览器访问本站。');
-    window.location.href='https://www.google.cn/chrome';
+    bodyEl.innerHTML = mqqHTML;
 } 
