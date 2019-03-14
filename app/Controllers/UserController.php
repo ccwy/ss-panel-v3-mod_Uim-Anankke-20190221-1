f3177a85308929427b2500be93811f5d46285f9a
ccwy <ccwy@users.noreply.github.com>
2019-03-14 15:04:30 +0800

    Update UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1081,13 +1081,6 @@ class UserController extends BaseController
             $res['msg'] = "非法请求";
             return $response->getBody()->write(json_encode($res));
         }
-	//流量加油包仅限年付用户购买
-	if ($shop->id == 68) { 
-	if ($user->class < 22 ) {
-            $res['ret'] = 0;
-            $res['msg'] = "流量加油包仅限年付用户购买";
-            return $response->getBody()->write(json_encode($res));
-        } }
 
         if ($coupon == "") {
             $credit = 0;
