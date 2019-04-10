df3ea1ff22b453c9590e4765ef9d89b0e9cec0e2
ccwy <ccwy@users.noreply.github.com>
2019-04-10 11:02:34 +0800

    Update CodeController.php

--- app/Controllers/Admin/CodeController.php
+++ app/Controllers/Admin/CodeController.php
@@ -91,7 +91,7 @@ class CodeController extends AdminController
     public function ajax_code($request, $response, $args)
     {
         $datatables = new Datatables(new DatatablesHelper());
-        $datatables->query('Select code.id,code.code,code.type,code.number,code.isused,code.userid,code.userid as user_name,user_namecode.usedatetime from code,user,WHERE code.userid = user.id and user_name = user.user_name');
+        $datatables->query('Select code.id,code.code,code.type,code.number,code.isused,code.userid,code.userid as user_name,user_namecode.usedatetime from code,user,WHERE code.user_name = user.user_name');
 
         $datatables->edit('number', function ($data) {
             switch ($data['type']) {
