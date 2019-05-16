e4641537137ad49d645ce19594c076473d781252
ccwy <ccwy@users.noreply.github.com>
2019-05-16 11:38:21 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -29,12 +29,22 @@ require BASE_PATH.'/config/routes.php';
 }
 */
 
-
+$ip = $_SERVER['REMOTE_ADDR'];
+$content = file_get_contents(‘http://ip.taobao.com/service/getIpInfo.php?ip=’.$ip);
+$banned = json_decode(trim($content), true);
+$lan = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+if((!empty($banned['data']['country_id']) && $banned['data']['country_id'] == ‘CN’) || strstr($lan, ‘zh’))
+{
+header(“HTTP/1.0 404 Not Found”);
+echo ‘HTTP/1.0 404 Not Found’;
+exit;
+}
+/*
 //  PUBLIC_PATH
 define('PUBLIC_PATH', __DIR__);
 // Bootstrap
 require PUBLIC_PATH.'/../bootstrap.php';
 // Init slim routes
 require BASE_PATH.'/config/routes.php';
-
+*/
 
