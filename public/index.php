15f480254e0657d4c5a528eb2a351805ad8336c0
ccwy <ccwy@users.noreply.github.com>
2019-06-11 09:06:16 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,10 +8,10 @@ $countryCode = geoip_country_code_by_addr($geoData, $_SERVER['REMOTE_ADDR']);
 geoip_close($geoData);  
 global $countryCode;  
 */
-
+/*
 //屏蔽中国ip，透过cf
 $countryCode = $_SERVER["HTTP_CF_IPCOUNTRY"];
-if($countryCode == 'CN') {
+if($countryCode == 'CNgdse') {
 if (substr($_SERVER["REQUEST_URI"],0, 9) === '/error_ip' or substr($_SERVER["REQUEST_URI"],0, 5) === '/cous' or substr($_SERVER["REQUEST_URI"], 0, 6) === '/link/') {	
 define('PUBLIC_PATH', __DIR__);
 require PUBLIC_PATH.'/../bootstrap.php';
@@ -25,13 +25,13 @@ define('PUBLIC_PATH', __DIR__);
 require PUBLIC_PATH.'/../bootstrap.php';
 require BASE_PATH.'/config/routes.php';
 }
+*/
 
-/*
 //  PUBLIC_PATH
 define('PUBLIC_PATH', __DIR__);
 // Bootstrap
 require PUBLIC_PATH.'/../bootstrap.php';
 // Init slim routes
 require BASE_PATH.'/config/routes.php';
-*/
+
 
