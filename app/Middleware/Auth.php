5cadb4eb72e509bfacdd978605f122989051382e
ccwy <ccwy@users.noreply.github.com>
2019-07-18 12:57:04 +0800

    Update Auth.php

--- app/Middleware/Auth.php
+++ app/Middleware/Auth.php
@@ -19,7 +19,7 @@ class Auth
             return $newResponse;
         }
 		
-        if ($user->enable == 0 && $_SERVER["REQUEST_URI"] != "/user/disable" && $_SERVER["REQUEST_URI"] != "/user/ticket" && $_SERVER["REQUEST_URI"] != "/user/ticket/create" && $_SERVER["REQUEST_URI"] != "/user/ticket/ticket_view") {
+        if ($user->enable == 0 && $_SERVER["REQUEST_URI"] == "/user/announcement" && $_SERVER["REQUEST_URI"] == "/user/bought" && $_SERVER["REQUEST_URI"] == "/user/code" && $_SERVER["REQUEST_URI"] == "/user/codefail" && $_SERVER["REQUEST_URI"] == "/user/detect_index" && $_SERVER["REQUEST_URI"] == "/user/detect_log" && $_SERVER["REQUEST_URI"] == "/user/edit" && $_SERVER["REQUEST_URI"] == "/user/fanli" && $_SERVER["REQUEST_URI"] == "/user/guide" && $_SERVER["REQUEST_URI"] == "/user/index" && $_SERVER["REQUEST_URI"] == "/user/invite" && $_SERVER["REQUEST_URI"] == "/user/node" && $_SERVER["REQUEST_URI"] == "/user/profile" && $_SERVER["REQUEST_URI"] == "/user/shop" && $_SERVER["REQUEST_URI"] == "/user/trafficlog") {
             $newResponse = $response->withStatus(302)->withHeader('Location', '/user/disable');
             return $newResponse;
         }
