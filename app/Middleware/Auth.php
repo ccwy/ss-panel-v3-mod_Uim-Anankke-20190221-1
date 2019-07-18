391c52d262b8609802e7a0ea90bf25f622b75b61
ccwy <ccwy@users.noreply.github.com>
2019-07-18 12:22:50 +0800

    Update Auth.php

--- app/Middleware/Auth.php
+++ app/Middleware/Auth.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ResponseInterface;
 use App\Services\Auth as AuthService;
 use App\Services\Config;
 use App\Models\Ticket;
-
+use App\Models\User;
 use App\Services\Jwt;
 
 class Auth
@@ -20,7 +20,9 @@ class Auth
             return $newResponse;
         }
 		
-        if ($user->enable == 0 && $_SERVER["REQUEST_URI"] != "/user/disable" && $_SERVER["REQUEST_URI"] != "/user/ticket" && $_SERVER["REQUEST_URI"] != "/user/ticket/create" && $_SERVER["REQUEST_URI"] != "/user/ticket/".$ticket->id."/view") {
+		$user_id = User::where("id", "=", $user_id)->first();
+		$tickeid = Ticket::where("id", "=", $tickeid)->Where("userid", "=", $user_id)->first();
+        if ($user->enable == 0 && $_SERVER["REQUEST_URI"] != "/user/disable" && $_SERVER["REQUEST_URI"] != "/user/ticket" && $_SERVER["REQUEST_URI"] != "/user/ticket/create" && $_SERVER["REQUEST_URI"] != "/user/ticket/".$tickeid."/view") {
             $newResponse = $response->withStatus(302)->withHeader('Location', '/user/disable');
             return $newResponse;
         }
