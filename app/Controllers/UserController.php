749d3acaf66865b05c677e2d82fbbf41b21e98d7
ccwy <ccwy@users.noreply.github.com>
2020-01-04 00:16:22 +0800

    Update UserController.php

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1925,7 +1925,7 @@ class UserController extends BaseController
         }
         if (!Tools::fanliss($code_olrid) || strlen($code_olrid) < 20) {
             $res['ret'] = 0;
-            $res['msg'] = "请输入完整付款订单号，付款订单号可以在支付宝/微信账单里面找到。";
+            $res['msg'] = "请输入完整付款订单号，付款订单号可以在支付宝/微信账单里面找到。付款订单号错误会影响工单处理速度哦。";
             return $this->echoJson($response, $res);
         }
 
